9381e4e4b23ef67d503c712ace29e173b66ce1e0
pantherpaykey <pantherpaykey@gmail.com>
2026-09-12 19:49:12 +0200

    Fix Deno 1.26 Discordeno import

--- bundled/deno/main.ts
+++ bundled/deno/main.ts
@@ -1,5 +1,24 @@
-import { createBot, Intents, startBot } from "discordeno";
+import {
+  createBot,
+  Intents,
+  startBot,
+} from "https://raw.githubusercontent.com/discordeno/discordeno/17.0.0/mod.ts";
+
 const token = Deno.env.get("DISCORD_TOKEN");
-if (!token) { console.error("DISCORD_TOKEN is not configured"); Deno.exit(1); }
-const bot = createBot({ token, intents: Intents.Guilds, events: { ready() { console.log("[Maszynka] Logged in"); } } });
+
+if (!token) {
+  console.error("DISCORD_TOKEN is not configured");
+  Deno.exit(1);
+}
+
+const bot = createBot({
+  token,
+  intents: Intents.Guilds,
+  events: {
+    ready() {
+      console.log("[Maszynka] Logged in");
+    },
+  },
+});
+
 await startBot(bot);
